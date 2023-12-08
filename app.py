5bda01000d2a732616795424ae03d4a044602616
unknown <chingu647@gmail.com>
2023-12-08 17:38:44 +0900

    app all...

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ if start_button:
     temp_df1 = temp_df1[ ( temp_df1[state] >= slider_range[0] ) & ( temp_df1[state] <= slider_range[1]) ] 
 
     con10.table(temp_df1) 
+    sbar.succedd('Filter Applied') 
+    sbar.balloons() 
 
 
 
