b9be782252b6e72dc7de60b572e52bfe20b34b5a
unknown <chingu647@gmail.com>
2023-12-08 11:26:28 +0900

    app all

--- app.py
+++ app.py
@@ -17,4 +17,10 @@ emp0, head0, emp1 = st.columns( [0.1, 1.0, 0.1] )
 emp0, con10, emp1 = st.columns( [0.1, 1.0, 0.1] )
 emp0, tail0,   emp1 = st.columns( [0.1, 1.0,0.1] ) 
 
+# 데이터 확인 
+df = pd.read_csv("data/iris.csv") 
+#con10.dataframe(df.columns)
+
+# 옵션 설정 
+
 
