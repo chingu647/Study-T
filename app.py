f7113ba9b7b4e64c3a53aef3cbca027aba9b398a
unknown <chingu647@gmail.com>
2023-12-08 21:03:21 +0900

    app all...

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ slider_range = sbar.slider('choose range key column', 0.0, 10.0, (2.5, 7.5) )
 # 버튼 
 start_button = sbar.button('filter apply 📊') 
 if start_button: 
-	temp_df2 = df0[df0.variety.isin(m_choice)]
+	temp_df2 = df0 #[df0.variety.isin(m_choice)]
 	temp_df2 = temp_df2[ ( temp_df2[state] >= slider_range[0] ) & ( temp_df2[state] <= slider_range[1]) ] 
 	
 	flag0 = False 
