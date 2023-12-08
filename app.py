b8ac97a5c8c07544c65583812c76dfcf2378a031
unknown <chingu647@gmail.com>
2023-12-08 11:19:16 +0900

    app all

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import time 
+import os 
+from datetime import datetime 
+from collections import Counter 
+from PIL import Image 
+
+st.set_page_config(layout="wide")
+
+st.markdown("#### Hello~~ Good Luck !!!")
+st.markdown("""---""") 
+
+# 컬럼 구성 
+emp0, head0, emp1 = st.columns( [0.1, 1.0, 0.1] )
+emp0, con10, emp1 = st.columns( [0.1, 1.0, 0.1] )
+emp0, tail0,   emp1 = st.columns( [0.1, 1.0,0.1] ) 
+
+# 데이터 확인 
+df = pd.read_csv("data/iris.csv") 
+con10.dataframe(df.columns)
+
+# 옵션 설정 
+
+
