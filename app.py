56908ec5fe48e846c19da9bc499cf27c95b6fc4f
unknown <chingu647@gmail.com>
2023-12-08 17:19:47 +0900

    app all...

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ sbar.title('Iris Species🌸')
 # 멀티셀렉트 
 my_mselect = list(sr_variety) 
 m_choice = sbar.multiselect('확인하고 싶은 종은? (복수선택 가능)', my_mselect) 
-temp_df1 = df0[df0.variety.isin(m_choice)].iloc[:,:-1] 
+temp_df1 = df0[df0.variety.isin(m_choice)]
 con10.dataframe(temp_df1) 
 
 # 라디오  
-my_rselect = list(df0.columns) 
+my_rselect = list(df0.columns[:-1]) 
 r_choice = sbar.radio("what is key column ?", my_rselect, horizontal=True) 
 
 # 슬라이더 
