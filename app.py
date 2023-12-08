006d8184c48961375aa520640e5d978568741d95
unknown <chingu647@gmail.com>
2023-12-08 20:28:35 +0900

    app all...

--- app.py
+++ app.py
@@ -43,10 +43,6 @@ my_mselect = list(sr_variety)
 m_choice = sbar.multiselect('확인하고 싶은 종은? (복수선택 가능)', my_mselect) 
 temp_df1 = df0[df0.variety.isin(m_choice)] 
 
-del temp_df1 
-
-temp_df1 = temp_df1 
-
 if not temp_df1.empty: 
 	con10.dataframe(temp_df1) 
 
@@ -61,7 +57,7 @@ if start_button:
 	temp_df2 = df0[df0.variety.isin(m_choice)]
 	temp_df2 = temp_df2[ ( temp_df2[state] >= slider_range[0] ) & ( temp_df2[state] <= slider_range[1]) ] 
 	temp_df1 = temp_df2
-#	con10.dataframe(temp_df1) 
+	con10.dataframe(temp_df1) 
 	sbar.success('Filter Applied') 
 	sbar.balloons() 
 
