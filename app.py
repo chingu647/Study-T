a1b4cba193f5e49a9f0f56702789a0b7c8d80c35
unknown <chingu647@gmail.com>
2023-12-08 20:56:28 +0900

    app all...

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ m_choice = sbar.multiselect('확인하고 싶은 종은? (복수선택 가능)',
 temp_df1 = df0[df0.variety.isin(m_choice)] 
 
 flag0 = True
-if m_choice != '' and flag0==True: 
+if m_choice != None & flag0==True: 
 	con10.dataframe(temp_df1) 
 
 # 라디오  
