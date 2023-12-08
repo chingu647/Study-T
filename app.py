f6283a4bfeb1d0bcf9ab3941b2498e37fa0293cb
unknown <chingu647@gmail.com>
2023-12-08 20:39:52 +0900

    app all...

--- app.py
+++ app.py
@@ -87,11 +87,12 @@ con11.map(map_data)
 latest_iteration = st.empty() 
 bar = st.progress(0) 
 
-for i in rnag(100):
-	latest_iteration.text(f"Iteration {i+1}")
-	bar.progress(i+1)
-	time.sleep(0.05) 
-st.balloons() 
+with con10: 
+	for i in range(100):
+		latest_iteration.text(f"Iteration {i+1}")
+		bar.progress(i+1)
+		time.sleep(0.05) 
+	st.balloons() 
 
 
 
